6c069feedd26581e5c71008ae1efcda9ed3a9dd9
Summer <su943515688@gmail.com>
2019-11-27 09:16:30 +0800

    [+] introduce

--- src/pages/frontend/index/index.tsx
+++ src/pages/frontend/index/index.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Article from '@/pages/frontend/index/components/Article/Article';
 import styles from './index.less';
+import Introduce from '@/components/Introduce';
 
 class Index extends Component {
   componentDidMount(): void {}
@@ -9,6 +10,7 @@ class Index extends Component {
     return (
       <div className={styles.mainInner}>
         <Article />
+        <Introduce />
       </div>
     );
   }
