ca9ad883a8b50c4569e90ea7774273cd11d2f625
Summer <su943515688@gmail.com>
2019-11-29 15:25:14 +0800

    [*] 优化播放器

--- src/components/Aplayer/index.tsx
+++ src/components/Aplayer/index.tsx
@@ -1,21 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 // @ts-ignore
 import ReactAplayer from 'react-aplayer';
 import request from 'umi-request';
 
 const Aplayer = () => {
-  let aplayerRef: any;
-
-  useEffect(() => {
-    const fetchData = async () => {
-      await request
-        .get('https://api.i-meto.com/meting/api?server=netease&type=playlist&id=35798529')
-        .then(response => {
-          aplayerRef.list.add(response);
-        });
-    };
-    fetchData();
-  }, []);
+  const [aplayerComponent, setAplayerComponent] = useState<React.FC>();
 
   const props = {
     theme: '#F57F17',
@@ -25,15 +14,20 @@ const Aplayer = () => {
     audio: [],
   };
 
-  const onInit = (ap: any) => {
-    aplayerRef = ap;
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      await request
+        .get('https://api.i-meto.com/meting/api?server=netease&type=playlist&id=35798529')
+        .then(response => {
+          props.audio = response;
+          const reactDom = ((<ReactAplayer {...props} />) as any) as React.FC;
+          setAplayerComponent(reactDom);
+        });
+    };
+    fetchData();
+  }, []);
 
-  return (
-    <div id="reactAplayer">
-      <ReactAplayer {...props} onInit={onInit} />
-    </div>
-  );
+  return <div id="reactAplayer">{aplayerComponent}</div>;
 };
 
 export default Aplayer;
