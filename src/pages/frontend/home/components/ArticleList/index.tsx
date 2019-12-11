25ee9243018a1123d1cd8dbcd3986f915b20f5c3
Summer <su943515688@gmail.com>
2019-12-11 17:42:35 +0800

    [*] 优化结构

--- src/pages/frontend/home/components/ArticleList/index.tsx
+++ src/pages/frontend/home/components/ArticleList/index.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { Card, Icon, Skeleton } from 'antd';
-import styles from './index.less';
+import { Skeleton } from 'antd';
+import styles from '../index.less';
 import classNames from 'classnames';
 import { ArticleListDataItemType } from '@/pages/frontend/home/data';
-import moment from 'moment';
 import ReactMarkdown from 'react-markdown';
 import Ellipsis from '@/components/Ellipsis';
+import ArticleSkeleton from '@/pages/frontend/home/components/ArticleSkeleton';
 
 interface ArticleProps {
   dataSource: ArticleListDataItemType[];
@@ -20,55 +20,20 @@ const Index: React.FC<ArticleProps> = ({ loading, dataSource, ...rest }) => {
     <Skeleton loading={loading}>
       <section className={classNames(styles.content, styles.clearfix)}>
         {dataSource.map(item => (
-          <Card className={styles.article} key={item.id} {...rest}>
-            <div className={styles.articleDate}>
-              <div className={styles.articleMonth}>{moment(item.created_at).format('M')}月</div>
-              <div className={styles.articleDay}>{moment(item.created_at).format('DD')}</div>
-            </div>
-            <div className={styles.badge}>
-              <span>
-                <a href="">
-                  <span>{item.article_type}</span>
-                </a>
-              </span>
-            </div>
-            <div className={styles.block}>
-              <div className={styles.articleHeader}>
-                <h1>
-                  <a href="">{item.title}</a>
-                </h1>
-                <div className={styles.postMeta}>
-                  <Icon type="calendar" style={{ color: '#00a7e0', marginRight: '5px' }} />
-                  <span className={styles.postTime}>
-                    发表于 {moment(item.created_at).format('Y-MM-DD')}
-                  </span>
-                  <span className={styles.wordCount}>
-                    <em>•</em>
-                    <Icon type="file-word" style={{ color: '#000', marginRight: '5px' }} />
-                    数字统计 832
-                  </span>
-                  <span className={styles.visitor}>
-                    <em>•</em>
-                    <Icon type="eye" style={{ color: '#ff3f1a', marginRight: '5px' }} />被{' '}
-                    {item.page_view}
-                    人看爆
-                  </span>
-                </div>
-              </div>
-              <div className={styles.articleBody}>
-                {articleState ? (
-                  <Ellipsis lines={6}>
-                    <ReactMarkdown source={item.content} escapeHtml={false} />
-                  </Ellipsis>
-                ) : (
+          <ArticleSkeleton item={item} {...rest}>
+            <div className={styles.articleBody}>
+              {articleState ? (
+                <Ellipsis lines={6}>
                   <ReactMarkdown source={item.content} escapeHtml={false} />
-                )}
-              </div>
-              <div className={styles.articleDetail}>
-                <a onClick={handleClickDetail}>阅读全文 »</a>
-              </div>
+                </Ellipsis>
+              ) : (
+                <ReactMarkdown source={item.content} escapeHtml={false} />
+              )}
             </div>
-          </Card>
+            <div className={styles.articleDetail}>
+              <a onClick={handleClickDetail}>阅读全文 »</a>
+            </div>
+          </ArticleSkeleton>
         ))}
       </section>
     </Skeleton>
---
+++ src/pages/frontend/home/components/ArticleSkeleton.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styles from './index.less';
+import moment from 'moment';
+import { Card, Icon } from 'antd';
+import { ArticleListDataItemType } from '@/pages/frontend/home/data';
+
+interface ArticleSkeletonProps {
+  item: ArticleListDataItemType;
+}
+
+const ArticleSkeleton: React.FC<ArticleSkeletonProps> = ({ item, children, ...rest }) => (
+  <Card className={styles.article} key={item.id} {...rest}>
+    <div className={styles.articleDate}>
+      <div className={styles.articleMonth}>{moment(item.created_at).format('M')}月</div>
+      <div className={styles.articleDay}>{moment(item.created_at).format('DD')}</div>
+    </div>
+    <div className={styles.badge}>
+      <span>
+        <a href="">
+          <span>{item.article_type}</span>
+        </a>
+      </span>
+    </div>
+    <div className={styles.block}>
+      <div className={styles.articleHeader}>
+        <h1>
+          <a href="">{item.title}</a>
+        </h1>
+        <div className={styles.postMeta}>
+          <Icon type="calendar" style={{ color: '#00a7e0', marginRight: '5px' }} />
+          <span className={styles.postTime}>
+            发表于 {moment(item.created_at).format('Y-MM-DD')}
+          </span>
+          <span className={styles.wordCount}>
+            <em>•</em>
+            <Icon type="file-word" style={{ color: '#000', marginRight: '5px' }} />
+            数字统计 832
+          </span>
+          <span className={styles.visitor}>
+            <em>•</em>
+            <Icon type="eye" style={{ color: '#ff3f1a', marginRight: '5px' }} />被 {item.page_view}
+            人看爆
+          </span>
+        </div>
+      </div>
+      {children}
+    </div>
+  </Card>
+);
+
+export default ArticleSkeleton;
